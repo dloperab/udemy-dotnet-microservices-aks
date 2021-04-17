fdf08867f2b24cfe17a5f031c8c0e38dc1198f1a
David Lopera <davidlopera@outlook.com>
2021-04-17 13:01:58 -0500

    Shopping.API with MongoDb added

---
+++ Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+using MongoDB.Driver;
+
+using Shopping.API.Data;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class ProductController
+  {
+    private readonly ProductContext _context;
+    private readonly ILogger<ProductController> _logger;
+
+    public ProductController(ProductContext context, ILogger<ProductController> logger)
+    {
+      _context = context;
+      _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Product>> Get()
+    {
+      return await _context
+                      .Products
+                      .Find(p => true)
+                      .ToListAsync();
+    }
+  }
+}
--- Shopping.API/Startup.cs
+++ Shopping.API/Startup.cs
@@ -1,15 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
-namespace Shopping.Client
+using Shopping.API.Data;
+
+namespace Shopping.API
 {
   public class Startup
   {
@@ -23,12 +21,13 @@ namespace Shopping.Client
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddHttpClient("ShoppingAPIClient", client =>
+      services.AddControllers();
+      services.AddSwaggerGen(c =>
       {
-        client.BaseAddress = new Uri("http://localhost:5000/");
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Shopping.API", Version = "v1" });
       });
 
-      services.AddControllersWithViews();
+      services.AddSingleton<ProductContext>();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,12 +36,9 @@ namespace Shopping.Client
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shopping.API v1"));
       }
-      else
-      {
-        app.UseExceptionHandler("/Home/Error");
-      }
-      app.UseStaticFiles();
 
       app.UseRouting();
 
@@ -50,9 +46,7 @@ namespace Shopping.Client
 
       app.UseEndpoints(endpoints =>
       {
-        endpoints.MapControllerRoute(
-                  name: "default",
-                  pattern: "{controller=Home}/{action=Index}/{id?}");
+        endpoints.MapControllers();
       });
     }
   }
